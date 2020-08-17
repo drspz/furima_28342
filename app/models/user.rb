812c50bd9921afd79883ea11df7d5df2641e20d4
drspz <spz0707k@gmail.com>
2020-08-17 22:53:10 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   has_many :items
 
   validates :nick_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :birth, presence: true
+
 end
