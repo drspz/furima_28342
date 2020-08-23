ab571ebf490c286d0dd52684c1ba92f540595784
drspz <spz0707k@gmail.com>
2020-08-23 11:59:46 +0900

    FactoryBot 修正

---
+++ spec/factories/items.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item do
+    name                       {"aaaaa"}
+    explanation                {"aaaaa"}
+    category_id                {"2"}
+    condition_id               {"2"}
+    postage_type_id            {"2"}
+    shipping_region_id         {"2"}
+    preparation_day_id         {"2"}
+    selling_price              {"2"}
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
-  factory :item do
-    name                       {"aaaaa"}
-    explanation                {"aaaaa"}
-    category_id                {"1"}
-    condition_id               {"1"}
-    postage_type_id            {"1"}
-    shipping_region_id         {"1"}
-    preparation_day_id         {"1"}
-    selling_price              {"1"}
+  factory :user do
+    nick_name                  {"taro"}
+    first_name                 {"abe"}
+    family_name                {"kentaro"}
+    first_name_kana            {"abe"}
+    family_name_kana           {"kentaro"}
+    email                      {"aaa@gmail.com"}
+    password                   {"000000"}
+    password_confirmation      {password}
+    birth                      {"0000"}
   end
 end
